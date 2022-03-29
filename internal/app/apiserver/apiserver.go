6c22fa5b65bbf638588246ce8e588553b9baebf2
Gogolev_vadim <gogolev.vadim92@gmail.com>
2022-03-29 22:04:27 +0700

    Исправил ошибку Apiserver Добавили мудаль logrus

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,9 +1,17 @@
 package apiserver
 
+import (
+  "github.com/sirupsen/logrus"
+)
+
+
+
 // Apiserver
 
 type APIServer struct {
   config *Config
+// Поле в качестве апи сервера
+  logger *logrus.Logger
 }
 
 // New
@@ -11,9 +19,32 @@ type APIServer struct {
 func New(config *Config) *APIServer  {
   return &APIServer {
   config: config,
+  // инициализация аписервера
+  logger: logrus.New(),
   }
 }
 
+// Start
 func (s *APIServer) Start() error  {
+  if err := s.configureLogger(); err != nil {
+    return err
+  }
+
+  s.logger.info("starting api server")
+
   return nil
+}
+
+// Функция которая конфиргурирует наш логгер
+
+func (s *APIServer) confureLogger() error {
+  level, err := logrus.ParseLeverl(s, config.Loglevel)
+  if err != nil {
+    return err
+  }
+
+  s.logger.SetLevel(level)
+
+  return nil
+
 }
